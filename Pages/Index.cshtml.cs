68999952d1c42426097d3db4c8e96104787c1a10
RehberOdhano <rehber.odhano30@gmail.com>
2023-03-09 12:16:39 +0500

    implemented data analytics ... chart on the home page if the user is admin...

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IMS.Data;
+using IMS.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IMS.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public Dictionary<string, int> revenueSubmitted;
+        public Dictionary<string, int> revenueApproved;
+        public Dictionary<string, int> revenueRejected;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
 
         public void OnGet()
         {
+            InitDict(ref revenueSubmitted);
+            InitDict(ref revenueApproved);
+            InitDict(ref revenueRejected);
 
+            var invoices = _context.Invoice.ToList();
+            Console.WriteLine(invoices.Count);
+
+            foreach (var invoice in invoices)
+            {
+
+                switch (invoice.Status)
+                {
+                    case InvoiceStatus.SUBMITTED:
+                        revenueSubmitted[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    case InvoiceStatus.APPROVED:
+                        revenueApproved[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    case InvoiceStatus.REJECTED:
+                        revenueRejected[invoice.InvoiceMonth] += (int)invoice.InvoiceAmount;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void InitDict(ref Dictionary<string, int> dict)
+        {
+            dict = new Dictionary<string, int>()
+            {
+                { "January",0 },
+                { "February",0 },
+                { "March",0 },
+                { "April",0 },
+                { "May",0 },
+                { "June",0 },
+                { "July",0 },
+                { "August",0 },
+                { "September",0 },
+                { "October",0 },
+                { "November",0 },
+                { "December",0 }
+            };
         }
     }
 }
